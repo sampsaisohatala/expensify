e9225db814090306191824ecd642e8f5dc71f3a3
Sampsa <sampsa.isohatala@gmail.com>
2020-12-03 17:30:28 +0200

    Add date to form with moment.js & react-dates calender

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+import 'react-dates/initialize';
 
 const ExpenseForm = () => {
    const [description, setDescription] = useState('');
-   const [note, setNote] = useState('');
    const [amount, setAmount] = useState('');
+   const [createdAt, setCreatedAt] = useState(moment());
+   const [calenderFocused, setCalenderFocused] = useState(false);
+   const [note, setNote] = useState('');
 
    const onDescriptionChange = (e) => {
       setDescription(e.target.value);
    };
-   const onNoteChange = (e) => {
-      setNote(e.target.value);
-   };
+
    const onAmountChange = (e) => {
       console.log('onAmountChange');
       const amount = e.target.value;
@@ -20,12 +24,25 @@ const ExpenseForm = () => {
       }
    };
 
+   const onDateChange = (createdAt) => {
+      setCreatedAt(createdAt);
+   };
+
+   const onFocusChange = ({ focused }) => {
+      setCalenderFocused(focused);
+   };
+
+   const onNoteChange = (e) => {
+      setNote(e.target.value);
+   };
+
    return (
       <div>
          <h3>Expense form</h3>
          <form>
             <input type="text" placeholder="Description" autoFocus value={description} onChange={onDescriptionChange} />
             <input type="number" placeholder="Amount" value={amount} onChange={onAmountChange} />
+            <SingleDatePicker date={createdAt} onDateChange={onDateChange} focused={calenderFocused} onFocusChange={onFocusChange} numberOfMonths={1} firstDayOfWeek={1} isOutsideRange={() => false} />
             <textarea placeholder="Add a note to your expense (optional)" value={note} onChange={onNoteChange}></textarea>
             <button>Add expense</button>
          </form>
