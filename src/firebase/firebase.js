f832648af84bc686d669b643126614ed8b7db764
Sampsa <sampsa.isohatala@gmail.com>
2020-12-15 16:01:47 +0200

    Setup environment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import Firebase from 'firebase';
 
 const firebaseConfig = {
-   apiKey: 'AIzaSyB3TrQDaPH_U7dn5SjngK0aQStLhr_31eY',
-   authDomain: 'expensify-e9230.firebaseapp.com',
-   databaseURL: 'https://expensify-e9230-default-rtdb.europe-west1.firebasedatabase.app',
-   projectId: 'expensify-e9230',
-   storageBucket: 'expensify-e9230.appspot.com',
-   messagingSenderId: '550358023096',
-   appId: '1:550358023096:web:64b56864671b271b9eed4d',
+   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Init firebase
