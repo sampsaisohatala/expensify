d8a11d5173b16506e2d15e2c80a62d278cc6f916
Sampsa <sampsa.isohatala@gmail.com>
2020-12-16 11:11:07 +0200

    Setup startRemoveExpense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { editExpense, removeExpense } from '../actions/expenses';
+import { editExpense, startRemoveExpense } from '../actions/expenses';
 
 const EditExpensePage = (props) => {
    return (
@@ -16,8 +16,7 @@ const EditExpensePage = (props) => {
          />
          <button
             onClick={() => {
-               console.log('remove', props.expense.id);
-               props.dispatch(removeExpense({ id: props.expense.id }));
+               props.dispatch(startRemoveExpense({ id: props.expense.id }));
                props.history.push('/');
             }}
          >
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,7 +1,7 @@
 import { createStore } from 'redux';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { addExpense, editExpense, removeExpense, startAddExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import { addExpense, editExpense, removeExpense, startAddExpense, setExpenses, startSetExpenses, startRemoveExpense } from '../../actions/expenses';
 import expensesTestData from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -121,5 +121,24 @@ test('should fetch expenses from firebase', (done) => {
          expenses: expensesTestData,
       });
       done();
+   });
+});
+
+test('should remove expenses from database', (done) => {
+   const store = createMockStore({});
+   const id = expensesTestData[1].id;
+   store.dispatch(startRemoveExpense({ id })).then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+         type: 'REMOVE_EXPENSE',
+         id,
+      });
+      return database
+         .ref(`expenses/${id}`)
+         .once('value')
+         .then((snapshot) => {
+            expect(snapshot.val()).toBeFalsy();
+            done();
+         });
    });
 });
